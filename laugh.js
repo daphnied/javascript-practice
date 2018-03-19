154c6205ec303dcdc760d790c110d58d4e53b7bd
D Donnell <daphniedonnell@gmail.com>
2018-03-19 17:43:24 -0400

    Added file

---
+++ laugh.js
@@ -0,0 +1,19 @@
+function laugh(num){
+    var ha = "";
+    for (var n = 0; n < num; n ++){
+        ha += "ha";
+    }
+    return ha + "!";
+}
+    console.log(laugh(3));
+
+
+    /*
+ * 
+ *
+ * Write a function called `laugh` with a parameter named `num` that represents the number of "ha"s to return.
+ *
+ * Note:
+ *  - make sure your the final character is an exclamation mark ("!")
+ *  - make sure that your function produces the correct results when it is called multiple times
+ */
