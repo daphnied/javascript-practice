6f589e64f4b879d487fa23dd588ed4d4ba26b85e
D Donnell <daphniedonnell@gmail.com>
2018-03-24 14:11:25 -0400

    Added file

---
+++ build-triangle.js
@@ -0,0 +1,24 @@
+/*
+ * Building a triangle
+ */
+
+// creates a line of * for a given length
+function makeLine(length) {
+    var line = "";
+    for (var j = 1; j <= length; j++) {
+        line += "* ";
+    }
+    return line + "\n";
+}
+
+
+function buildTriangle(width) {
+    var triangle="";
+    for (var i=1; i<=width; i++){
+        triangle += (makeLine(i));
+    }
+    return triangle;
+}
+buildTriangle(10);
+
+console.log(buildTriangle(10));
